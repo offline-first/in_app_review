54755b2824faf48146411baeb8bca99e60dbe0b0
Marian <info@offlinefirst.net>
2024-09-10 10:42:55 +0300

    last state

--- in_app_review/android/src/main/java/dev/britannio/in_app_review/InAppReviewPlugin.java
+++ in_app_review/android/src/main/java/dev/britannio/in_app_review/InAppReviewPlugin.java
@@ -13,7 +13,7 @@ import androidx.annotation.NonNull;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
-import com.google.android.play.core.tasks.Task;
+import com.google.android.gms.tasks.Task;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
